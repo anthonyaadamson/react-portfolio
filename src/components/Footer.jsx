8f3ed7c7fdc9581b5c85b07521425b58cc0a9d48
unknown <anthonyaadamson@gmail.com>
2023-12-12 20:54:54 -0500

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer className="position-fixed bottom-0 start-50 translate-middle-x">
+        <footer className="position-relative bottom-0 start-50 translate-middle-x">
             <a href="https://github.com/anthonyaadamson"><img src="github_logo.png" alt="Github Logo" /></a>
             <a href="https://indeed.com"><img src="indeed_logo.png" alt="Indeed Logo" /></a>
         </footer>
